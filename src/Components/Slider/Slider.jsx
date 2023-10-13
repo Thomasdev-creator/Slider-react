98103320587e89b632c5c4ff298870d92c647dbe
Thomas Fournier <thomasfournierbrest@gmail.com>
2023-10-13 11:52:40 +0200

    add component, css, media query

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,21 +1,21 @@
 import leftChevron from "../../assets/left-arrow.svg"
 import rightChevron from "../../assets/right-arrow.svg"
 import "./Slider.css"
+
 export default function Slider() {
   return (
     <>
     <p className="index-info">3/5</p>
     <div className="slider">
         <p className="image-info">Bedroom</p>
-        <img src="/images/img-3.jpg" alt="estate'srooms" className="slider-img" />
+        <img src="/images/img-3.jpg" alt="estate's rooms" className="slider-img" />
 
         <button className="navigation-button prev-button">
             <img src={leftChevron} alt="previous image" />
         </button>
-        <button className="navigation-button prev-button">
+        <button className="navigation-button next-button">
             <img src={rightChevron} alt="next image" />
         </button>
-        
     </div>
     </>
   )
